c245c3990593988e4082b7d92293136d5660a763
eahishakiye <ahishema@gmail.com>
2024-03-21 09:43:09 +0300

    Added doc comments for each of the functions

--- assignment2.py
+++ assignment2.py
@@ -1,17 +1,19 @@
-#Assignment - Ahishakiye Emmanuel
+# Assignment - Ahishakiye Emmanuel
 # The name of the file is assignment2.py
-# In the code below, three functions are created. 
+# In the code below, three functions are created.
 
 # in the first function, a net salary is created, given a gross salary
 def salary(gross):
+    """"A function for calculating net salary from gross salary""" 
     paye = 0.3 * gross
     nssf = 0.05 * gross
     net_pay = gross - (paye + nssf)
     return net_pay
 
+
 # In the following function, a net balance on the account is calculated given stated expenses
 def expenses(fees):
-    #Net balance after fees payment
+    """Net balance after fees payment"""
     rent = 600000
     water = 100000
     electricity = 100000
@@ -19,8 +21,10 @@ def expenses(fees):
     internet = 100000
     fuel = 500000
     food = 300000
-    net_balance = salary(6000000) - (fees + rent + water + electricity + phone + internet + fuel + food)
+    net_balance = salary(6000000) - (fees + rent + water +
+                                     electricity + phone + internet + fuel + food)
     return net_balance
+
 
 # in the following function, the balance at the end of the month after expenses is calculated
 def actual_balance():
@@ -28,5 +32,6 @@ def actual_balance():
     party = 300000
     balance = expenses(1500000) - (tithe + party)
     print(balance)
-    
+
+
 actual_balance()
